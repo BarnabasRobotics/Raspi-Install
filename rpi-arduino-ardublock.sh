b03181562264c0f1574715296a50aba8b66c12c8
Andrew <andrew@barnabasrobotics.com>
2020-04-29 14:02:43 -0700

    add install script

---
+++ rpi-arduino-ardublock.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# This script downloads the arduino tarball
+# extracts the tarball
+# installs arduino
+# installs ardublock
+# installs the library
+# removes the tarball
+# removes the install files
+
+wget -P $HOME/Downloads https://downloads.arduino.cc/arduino-1.8.12-linuxarm.tar.xz
+tar -xvf $HOME/Downloads/arduino-1.8.12-linuxarm.tar.xz -C $HOME
+cd $HOME/arduino-1.8.12/
+mkdir tools/ArduBlockTool/tool
+sudo ./install.sh
+
+
+wget -P $HOME/arduino-1-8-12/tools/ArduBlockTool/tool \
+    https://github.com/BarnabasRobotics/BarnabasRoboticsArdublock-2019/raw/master/bin/ardublock-all-withNewPing.jar
+
+wget -P $HOME/Downloads https://bitbucket.org/teckel12/arduino-new-ping/downloads/NewPing_v1.9.1.zip
+xarchiver -x $HOME/arduino-1.8.12/libraries $HOME/Downloads/NewPing_v1.9.1.zip
+
+# It might happen that, you will get an error “Error opening serial port”
+# while uploading a sketch after you have selected your board and the serial port.
+# To fix this error, run the following command
+
+# sudo usermod -a -G dialout $USER
